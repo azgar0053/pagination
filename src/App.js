4a6ee904437a970890b47d19620f69ba23343d01
azgar0053 <azgar53ali@gmail.com>
2024-01-07 01:06:17 +0530

    create react and added sample pagination

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [apiData, setApiData] = useState([]);
+  const [currentPage, setCurrentPage]= useState(1);
+  const itemsPerPage = 10;
+
+  const fetchData=async()=>{
+    const fetchD = await fetch('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json');
+    const res = await fetchD.json();
+    setApiData(res);
+  }
+
+  useEffect(()=>{
+    fetchData()
+  }, [])
+
+  const indexOfLastItem =currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = apiData.slice(indexOfFirstItem, indexOfLastItem);
+
+
+  const handlePrev=()=>{
+    setCurrentPage((prev)=> (prev>1? prev-1: prev ));
+  }
+  const handleNext=()=>{
+    setCurrentPage((prev)=> (apiData.length/prev>10?prev+1:prev));
+  }
+
+  return (
+    <div className="App">
+      <h1>Employee Data Table</h1>
+      <table style={{border:'2px solid lightGreen',borderCollapse:'collapse', width: '100%', margin:'20px 0px'}}>
+        <tr style={{border:'1px solid black',borderCollapse:'collapse', backgroundColor:'lightGreen', color:'white', }}>
+          <th style={{padding:'10px'}}>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Role</th>
+        </tr>
+        {currentItems.map((ele)=> 
+        <tr key={ele.id} style={{border:'1px solid black',borderCollapse:'collapse'}}>
+          <td style={{padding:'10px'}}>{ele.id}</td>
+          <td>{ele.name}</td>
+          <td>{ele.email}</td>
+          <td>{ele.role}</td>
+        </tr>)}
+      </table>
+      <div style={{display:'flex', justifyContent:'center',}}>
+        <button onClick={handlePrev} style={{margin:'10px', padding:'10px 20px'}}>Prev</button>
+        <p style={{margin:'10px'}}>{currentPage}</p>
+        <button onClick={handleNext} style={{margin:'10px', padding:'10px 20px'}}>Next</button></div>
+    </div>
+  );
+}
+
+export default App;
